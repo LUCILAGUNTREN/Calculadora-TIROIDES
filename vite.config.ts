29256f239feceb3461489ba52fec30b511ba2e9b
LUCILAGUNTREN <lucilaguntren@gmail.com>
2025-10-30 00:13:15 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // DEBE ESTAR: Añade esta línea si falta o revisa que esté correcta
   base: '/', 
   plugins: [react()],
 })
+
